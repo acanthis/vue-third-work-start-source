baa713a79f9e1df951d80e978c67a32ae788abdf
Artem Nikitin <a.nikitin@4payments.com>
2023-06-06 23:46:55 +0500

    new demo

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="counter">
+        <counter-button @action="decrement">-</counter-button>
+        <counter-value :value="count"/>
+        <counter-button @action="increment">+</counter-button>
+    </div>
+</template>
+
+<script setup>
+import CounterButton from "./CounterButton.vue";
+import CounterValue from "./CounterValue.vue";
+import {ref} from 'vue';
+
+const count = ref(0);
+const decrement = () => count.value--;
+const increment = () => count.value++;
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,1 +1,12 @@
-export default []
+export default [
+    {
+        name: 'Counter Base 0',
+        path: '/counter-0',
+        component: () => import("./demo0/DemoCounter.vue"),
+    },
+    {
+        name: 'Counter Base 1',
+        path: '/counter-1',
+        component: () => import("./demo1/DemoCounter.vue"),
+    },
+];
